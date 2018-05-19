5a3696528ff28046fe34ed3c71ea15a4cc8e1d9c
sovrinbloc <josephalai@gmail.com>
2018-05-18 18:10:02 -0700

    First Custom Vue Component inside Laravel 5.6

---
+++ resources/assets/js/components/Instructions.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>Vue Components Instructions</h3>
+        <h4>This will contain the directions for how to add components to Laravel.</h4>
+        <ol>
+            <li v-for="instruct in instructions"> {{ instruct }}</li>
+        </ol>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name : "test",
+    data() {
+      return  {
+        instructions: ['Create the component', 'Wire it up to vue.js', 'embed it inside your blade file', 'run a `npm run dev`',
+        'possibly clear the cache with `php artisan cache:clear`, or `php artisan view:clear`', 'Open an incognito.'],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- resources/assets/js/vue.js
+++ resources/assets/js/vue.js
@@ -5,8 +5,8 @@ Vue.config.productionTip = false;
 Vue.component('comment', require('./components/comments/Comment.vue'));
 Vue.component('comment-list', require('./components/comments/Comment-list.vue'));
 Vue.component('comment-form', require('./components/comments/Comment-form.vue'));
-
 Vue.component('like', require('./components/Like.vue'));
+Vue.component('instructions', require('./components/Instructions.vue'));
 
 window.Event = new Vue();
 
